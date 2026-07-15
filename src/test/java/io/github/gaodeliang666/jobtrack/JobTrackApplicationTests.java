add556fdd26a34daabaa037d5f3e4b3f33cd0e98
gaodeliang666 <1460327834@qq.com>
2026-07-15 16:01:43 +0800

    chore: prepare JobTrack v0.3.0 database foundation

--- src/test/java/io/github/gaodeliang666/jobtrack/JobTrackApplicationTests.java
+++ src/test/java/io/github/gaodeliang666/jobtrack/JobTrackApplicationTests.java
@@ -2,9 +2,17 @@ package io.github.gaodeliang666.jobtrack;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import io.github.gaodeliang666.jobtrack.user.mapper.AppUserMapper;
 
 @SpringBootTest
+@ActiveProfiles("no-db-test")
 class JobTrackApplicationTests {
+
+    @MockitoBean
+    private AppUserMapper appUserMapper;
 
     @Test
     void contextLoads() {
